2194c708388cd9ae24235f392bc23f0bff019677
mudit04 <muditmangal05@gmail.com>
2023-08-19 23:58:27 +0530

    db change

--- database/migration/000001_init_mg.up.sql
+++ database/migration/000001_init_mg.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user` (
   `userID` int NOT NULL AUTO_INCREMENT,
-  `Uname` varchar(255) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
   `admin` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`userID`)
@@ -12,13 +12,13 @@ CREATE TABLE `books` (
   PRIMARY KEY (`bookID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `request` (
-  `reqID` int NOT NULL AUTO_INCREMENT,
-  `UID` int DEFAULT NULL,
+  `requestID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
   `BookID` int DEFAULT NULL,
   `Status` int DEFAULT NULL,
-  PRIMARY KEY (`reqID`),
-  KEY `UID` (`UID`),
+  PRIMARY KEY (`requestID`),
+  KEY `UserID` (`UserID`),
   KEY `BookID` (`BookID`),
-  CONSTRAINT `request_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`userID`),
+  CONSTRAINT `request_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`userID`),
   CONSTRAINT `request_ibfk_2` FOREIGN KEY (`BookID`) REFERENCES `books` (`bookID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
